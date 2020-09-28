62b1ff4f69643d7d3988ac262aec7e0fcb7bbd56
Stuart Gray <stuart.gray@reachplc.com>
2020-09-28 15:17:17 +0100

    fix

--- App.js
+++ App.js
@@ -1,13 +1,25 @@
 import 'react-native-gesture-handler';
 
 import React from 'react';
-import {StyleSheet} from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 
-const App: () => React$Node = () => {
-  return <NavigationContainer />;
+const App = () => {
+  return (
+    <NavigationContainer>
+      <View style={styles.container}>
+        <Text>Hello</Text>
+      </View>
+    </NavigationContainer>
+  );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
 
 export default App;
